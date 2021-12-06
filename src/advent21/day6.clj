6013542003f5ea63b154cc23e157a040f2fd0b60
Rithwik K <0rithwik@gmail.com>
2021-12-06 12:16:19 +0530

    day 6

--- src/advent21/day6.clj
+++ src/advent21/day6.clj
@@ -1,11 +1,39 @@
 (require '[clojure.string :as str])
 
-(def sample
-  (->>
-""
-str/split-lines))
-
 (def input
-  (->> "resources/day6.txt"
-       slurp
-       str/split-lines))
+  (as-> "resources/day6.txt" x
+    (slurp x)
+    (str/split x #",")
+    (map read-string x)))
+
+;; part 1
+
+(defn transition-swarm [xs]
+  (flatten (map #(if (= % 0) [6 8] (dec %)) xs)))
+
+(->> input
+     (iterate transition-swarm)
+     (take 81)
+     last
+     count)
+
+;; part 2
+
+(defn transition-state [state]
+  {0 (get state 1 0)
+   1 (get state 2 0)
+   2 (get state 3 0)
+   3 (get state 4 0) 
+   4 (get state 5 0) 
+   5 (get state 6 0) 
+   6 (+ (get state 7 0) (get state 0 0))
+   7 (get state 8 0)
+   8 (get state 0 0)})
+
+(->> input
+     frequencies
+     (iterate transition-state)
+     (take 257)
+     last
+     (map second)
+     (apply +))
