1e9ec73eacc5bb92f73a787c5a0011495036cc76
Rithwik K <0rithwik@gmail.com>
2021-12-11 22:13:18 +0530

    cleanup

--- src/advent21/day05.clj
+++ src/advent21/day05.clj
@@ -1,23 +1,14 @@
 (require '[clojure.string :as str])
 
-(def sample
-  (->>
-"0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2"
-str/split-lines
-(map #(str/split % #" -> "))
-(map (fn [line]
-       (->> line
-            (map #(str/split % #","))
-            (map (fn [p] (map read-string p))))))))
+(def input
+  (->> "resources/day5.txt"
+       slurp
+       str/split-lines
+       (map #(str/split % #" -> "))
+       (map (fn [line]
+              (->> line
+                   (map #(str/split % #","))
+                   (map (fn [p] (map read-string p))))))))
 
 (defn get-sequence [x y]
   (cond
@@ -37,24 +28,6 @@ str/split-lines
         [x2 y2] p2]
     (or (= x1 x2)
         (= y1 y2))))
-
-(->> sample
-     ;; (filter not-diagonal?)
-     (map points-on-line)
-     (apply concat)
-     frequencies
-     (filter #(> (second %) 1))
-     count)
-
-(def input
-  (->> "resources/day5.txt"
-       slurp
-       str/split-lines
-       (map #(str/split % #" -> "))
-       (map (fn [line]
-              (->> line
-                   (map #(str/split % #","))
-                   (map (fn [p] (map read-string p))))))))
 
 (->> input
      ;; (filter not-diagonal?)
